99881970cd83a8862bd7f51035fd11b9fb2d0c96
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-10-21 22:09:34 +0530

    Switch Case

---
+++ geeksforgeeks/SwitchCase.java
@@ -0,0 +1,39 @@
+package geeksforgeeks;
+
+public class SwitchCase {
+
+    // User function Template for Java
+
+class Solution{
+    static double switchCase(int choice, List<Double> arr){
+        // code here
+        
+           double area=0;
+
+    // code here
+
+            switch(choice)
+
+            {
+
+            case 1:
+
+              area =   Math.PI*arr.get(0)*arr.get(0);// area of circle
+
+              break;
+
+              case 2:// area of rectangle
+
+        area = arr.get(0)*arr.get(1);
+
+                break;
+
+                
+
+            }
+
+            return area;
+    }
+}
+    
+}
