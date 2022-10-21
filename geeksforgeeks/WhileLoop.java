abb837fae5bcc1ad638effccc809dc75d1d67c7a
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-10-21 22:14:15 +0530

    While Loop

---
+++ geeksforgeeks/WhileLoop.java
@@ -0,0 +1,23 @@
+package geeksforgeeks;
+
+public class WhileLoop {
+    
+    //User function Template for Java
+class Geeks {
+    static void printTable (int n) 
+    {
+           int multiplier=10;
+           while(multiplier>0)
+           {
+               //Your code here
+               int num = n * multiplier;
+              
+            System.out.print(num + " ");
+           multiplier--;
+           
+           }
+      System.out.println();
+    }
+
+}
+}
