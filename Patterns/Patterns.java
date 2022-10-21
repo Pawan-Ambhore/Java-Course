5759adfbafbc1b90c4d9c4bd307880fc135d49f0
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-10-21 22:25:17 +0530

    Added Pattern Problems

---
+++ Patterns/Patterns.java
@@ -0,0 +1,84 @@
+package Patterns;
+
+public class Patterns {
+
+    //1
+
+ 
+
+    //     void printSquare(int n) {
+    //         // code here
+    //         for(int i = 0; i<n; i++){
+    //             for(int j = 0; j<n; j++){
+    //                  System.out.print("*"+" ");
+    //             }
+    //            System.out.println();
+    //         }
+            
+    //     }
+
+
+    //2
+ 
+//     void printTriangle(int n) {
+//         // code here
+//         for(int i = 0;i < n; i++){
+//             for(int j=0 ; j<=i; j++){
+                
+//                 System.out.print("*" + " ");
+                
+//             }
+//             System.out.println();
+//         }
+
+// }
+
+    //3
+
+    // void printTriangle(int n) {
+    //     // code here
+        
+    //     for(int i = 1; i<=n; i++){
+            
+    //         for(int j = 1; j<=i; j++){
+    //             System.out.print(j + " ");
+    //         }
+            
+            
+    //         System.out.println();
+    //     }
+    // }
+
+    //4
+
+    // void printTriangle(int n) {
+    //     // code here
+    //     for(int i = n; i>=1; i--){
+            
+    //         for(int j=1; j<=i; j++){
+                
+    //             System.out.print("*" + " ");
+    //         }
+            
+    //         System.out.println();
+    //     }
+    // }
+
+    //6
+
+    void printTriangle(int n) {
+        // code here
+        
+        for(int i =n; i>=1; i--){
+            
+            for(int j = 1; j<=i; j++){
+                System.out.print(j+ " ");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+
+    
+}
