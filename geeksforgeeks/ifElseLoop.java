c9ad30d300d07060473b0ab581d1685864d87f76
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-10-21 22:02:09 +0530

    If else

---
+++ geeksforgeeks/ifElseLoop.java
@@ -0,0 +1,21 @@
+package geeksforgeeks;
+
+public class ifElseLoop {
+
+    //User function Template for Java
+class Solution{
+    static String compareNM(int n,int m){
+        // code here
+        
+        if(n < m){
+            return "lesser";
+        }else if(n == m){
+            return "equal";
+        }else{
+            return "greater";
+        }
+
+    }
+}
+    
+}
