08d7fd37988dc93df4294f52cf9fbf99dcd86a49
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-10-25 15:45:59 +0530

    fibonacci series

---
+++ Coditions/fibonacciSeries.java
@@ -0,0 +1,30 @@
+package Coditions;
+
+import java.util.Scanner;
+
+public class fibonacciSeries {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+
+        int count = 2;
+
+        while(count <= n){
+
+            int temp = b;
+
+            b = b + a;
+            a  = temp;
+            count++;
+        }
+        System.out.println(b);
+        
+    }
+    
+}
