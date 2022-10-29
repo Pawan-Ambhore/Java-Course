b6981dc27d8ce18a7cfd152340205d96131e8b79
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-10-29 09:43:34 +0530

    Binary Search

---
+++ binarySearch/BinarySearch.java
@@ -0,0 +1,38 @@
+package binarySearch;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr  = {1, 23, 33, 56, 98, 802};
+
+        int target = 802;
+
+        int ans = search(arr, target);
+
+        System.out.println(ans);
+        
+    }
+
+    static int search(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }else if(target > arr[mid] ){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+    
+}
