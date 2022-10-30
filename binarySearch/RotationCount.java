435f56062ee8d6bda23f98635f19dc804d3ed2d7
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-10-30 16:12:59 +0530

    Binary Search in 1D Array

---
+++ binarySearch/RotationCount.java
@@ -0,0 +1,42 @@
+package binarySearch;
+
+public class RotationCount {
+
+    public static void main(String[] args) {
+        int[] arr= {4,5,7,0,1,2,3};
+        System.out.println(count(arr));
+    }
+    
+    static int count(int[] arr){
+      
+        int pivot = findcount(arr);
+        return pivot + 1;
+    }
+
+    
+    static int findcount(int[] arr){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+
+            if(mid > start && arr[mid] < arr[mid - 1]){
+                return mid - 1;
+            }
+
+            if(arr[mid] <= arr[start]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    } 
+}
