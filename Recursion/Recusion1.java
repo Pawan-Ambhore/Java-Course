e4acb56ce44635f582bf4b36560264be62a7716c
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-11-05 14:53:25 +0530

    Print 1 to 5 in recursion

---
+++ Recursion/Recusion1.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Recusion1 {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    public static void print(int n) {
+        if(n == 5){
+           System.out.println(5);
+           return;
+        }
+
+        System.out.println(n);
+        print(n + 1);
+    }
+}
