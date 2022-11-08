c569e56c5774edfd5c7da7e41efb541ea5137e1c
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-11-08 15:45:23 +0530

    Find number of digits

---
+++ numberSystems/poweOfTwo.java
@@ -0,0 +1,11 @@
+package numberSystems;
+
+public class poweOfTwo {
+    public static void main(String[] args) {
+        int n  = 31;
+
+        boolean ans = (n & (n - 1)) == 0;
+
+        System.out.println(ans);
+    }
+}
