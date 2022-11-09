823f4996e8ba2af8f55b9397c6a5755355c3c16a
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-11-09 15:24:50 +0530

    Calculate the um upto n

---
+++ RecursionEasy/Sum.java
@@ -0,0 +1,20 @@
+package RecursionEasy;
+
+public class Sum {
+    
+    public static void main(String[] args) {
+        int ans = sum(5);
+        System.out.println(ans);
+    }
+
+    static int sum(int n){
+    
+        if(n < 1){
+           return 0;
+        }
+
+        return n + sum(n - 1);
+
+
+    }
+}
